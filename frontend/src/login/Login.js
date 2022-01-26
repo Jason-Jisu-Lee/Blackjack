b854faf784005e69e9f03075e0407f76dbcf4383
Jason Lee <idforvy@gmail.com>
2022-01-26 05:51:21 -0500

    added backend and created login component

--- frontend/src/login/Login.js
+++ frontend/src/login/Login.js
@@ -1,30 +1,73 @@
-import React from "react";
-import "./login.css"
+import React, { useState } from "react";
+import "./login.css";
+import md5 from "md5";
 
 function Login() {
+  const [form, setForm] = useState({
+    username: "username",
+    password: "password",
+  });
+
+  const handleInputChange = (event) => {
+    const target = event.target;
+    let value = event.target.value;
+    const name = target.name;
+
+    if (target.name === "password") {
+      document.getElementById(name).type = "password";
+      value = md5(event.target.value);
+    }
+
+    setForm({
+      [name]: value,
+    });
+
+    document.getElementById(name).style.fontFamily = "Montserrat black";
+  };
+
+  const handleFocus = (event) => {
+    if (event.target.value === event.target.name) {
+      event.target.value = "";
+    }
+  };
+
+  const handleBlur = (event) => {
+    if (event.target.value === "") {
+      event.target.value = event.target.name;
+    }
+  };
+
   return (
     <div className="login">
-      <h4>Login</h4>
+      <h4>Welcome back</h4>
       <form>
         <div className="text_area">
           <input
             type="text"
             id="username"
             name="username"
-            defaultValue="username"
+            defaultValue={form.username}
+            onChange={handleInputChange}
+            onFocus={handleFocus}
+            onBlur={handleBlur}
             className="text_input"
           />
         </div>
         <div className="text_area">
           <input
-            type="password"
+            type="text"
             id="password"
             name="password"
-            defaultValue="password"
+            defaultValue={form.password}
+            onChange={handleInputChange}
+            onFocus={handleFocus}
+            onBlur={handleBlur}
             className="text_input"
           />
         </div>
-        <input type="submit" value="LOGIN" className="btn" />
+        <button type="submit" className="btn">
+          Log In
+        </button>
       </form>
       <a className="link" href="/signup">
         Sign Up
