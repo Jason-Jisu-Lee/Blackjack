a02637ad67a584c0bc973dc9ca892b3e430cd13b
Jason Lee <idforvy@gmail.com>
2022-01-31 03:29:26 -0500

    added Bootstrap and fixed body background color margin issue

--- frontend/src/play/Dealer.js
+++ frontend/src/play/Dealer.js
@@ -1,18 +1,34 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import cardGenerator from "../utils/cardGenerator";
 
 function Dealer({ playing }) {
-  // const [dealer, setDealer] = useState(null);
+  const [dealer, setDealer] = useState([]);
 
-  const dealerArray = [];
+  useEffect(() => {
+    while (dealer.length < 2) {
+      const generate = setTimeout(() => {
+        setDealer((prevState) => [...prevState, cardGenerator()]);
+      }, 1000);
+      return () => generate;
+    }
+  }, [dealer]);
+
+  const dealerHand = (
+    <div className="container">
+      <div className="row justify-content-center">
+        <div className="col-2">{dealer[0]}</div>
+        <div className="col-2">{dealer[1]}</div>
+      </div>
+    </div>
+  );
+
+  // If the player is playing, deal hands to the dealer
   if (playing) {
-    dealerArray.push(cardGenerator());
-    dealerArray.push(cardGenerator());
   }
 
   return (
     <Fragment>
-      <div>{playing ? dealerArray : "dealer"}</div>
+      <div>{playing ? dealerHand : "Dealer"}</div>
     </Fragment>
   );
 }
--- frontend/src/play/Player.js
+++ frontend/src/play/Player.js
@@ -1,9 +1,36 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect, useState } from "react";
+import cardGenerator from "../utils/cardGenerator";
 
-function Player() {
-  // const [player, setPlayer] = useState(null);
+function Player({ playing }) {
+  const [player, setPlayer] = useState([]);
 
-  return <Fragment>player</Fragment>;
+  useEffect(() => {
+    while (player.length < 2) {
+      const generate = setTimeout(() => {
+        setPlayer((prevState) => [...prevState, cardGenerator()]);
+      }, 1000);
+      return () => generate;
+    }
+  }, [player]);
+
+  const playerHand = (
+    <div className="container">
+      <div className="row justify-content-center">
+        <div className="col-2">{player[0]}</div>
+        <div className="col-2">{player[1]}</div>
+      </div>
+    </div>
+  );
+
+  // If the player is playing, deal hands to the player
+  if (playing) {
+  }
+
+  return (
+    <Fragment>
+      <div>{playing ? playerHand : "Player"}</div>
+    </Fragment>
+  );
 }
 
 export default Player;
